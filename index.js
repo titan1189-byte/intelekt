7b01ec1315214a8d8f4a693e200cf737dfa24c70
titan1189-byte <titan1189-byte@users.noreply.github.com>
2026-07-02 18:34:24 +0300

    відкрити Firebase Function для hosting API

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ exports.app = onRequest(
   {
     region: process.env.FUNCTION_REGION || "europe-west1",
     timeoutSeconds: 120,
-    memory: "512MiB"
+    memory: "512MiB",
+    invoker: "public"
   },
   requestHandler
 );
